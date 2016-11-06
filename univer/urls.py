8dcc0877edff26f4d43fa7eb8f4d5b841908b544
35359595 <35359595i@gmail.com>
2016-11-06 12:51:55 +0100

    main functionality compleete

--- univer/urls.py
+++ univer/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^students/', views.students, name='students'),
     url(r'^univers/', views.univer, name='univers'),
     url(r'^search/', views.search, name='search'),
-    #(?P<search_request>\w+)/$
     url(r'^(?P<s_id>[0-9]+)/$', views.student, name='student'),
+    url(r'^add/', views.add, name='add'),
+    url(r'^add_student/', views.add_student, name="add_student"),
 ]
--- univer/views.py
+++ univer/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.db.models import Q
-from .models import Student, University
+from .models import Student, University, Cathedra, HomeTask, Disciplin
 
 def index(request):
     template = loader.get_template('univer/index.html')
@@ -40,3 +40,22 @@ def search(request):
     context = { 'search_form': result, }
     return render(request, 'univer/index.html', context)
     #return HttpResponse(template.render(context, request))
+
+def add(request):
+    return render(request, 'univer/add.html')
+
+def add_student(request):
+    name = request.GET['add_name']
+    last_name = request.GET['add_last_name']
+    phone = request.GET['add_phone']
+    mail = request.GET['add_mail']
+    cath = Cathedra.objects.get(id=2)
+    uni = University.objects.get(id=1)
+    ht = HomeTask.objects.get(id=1)
+    dc = Disciplin.objects.get(id=2)
+    stud = Student(s_name=name, s_las_name=last_name, s_email=mail, s_phone=phone, 
+            s_cathedra=cath, s_university=uni, s_disciplins=dc, s_ht=ht)
+    stud.save()
+    students_list = Student.objects.order_by('s_name')
+    context = { 'students_list': students_list, }
+    return render(request, 'univer/index.html', context)
